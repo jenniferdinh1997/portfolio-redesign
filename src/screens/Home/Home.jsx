e25e12c1e355f4bac743c8f06a99ceb27e0ffe88
Jennifer Dinh <jenniferdinh1997@gmail.com>
2024-09-19 13:23:50 -0700

    portfolio update

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -49,10 +49,10 @@ const Home = () => {
       <div className="about-container">
         <h1 id="about-me">About Me</h1>
         <p>
-          Back in 2020 I graduated college and had to ask myself what I wanted
+          Back in 2020 I graduated UC San Diego and had to ask myself what I wanted
           to do with the rest of my life that would bring me both joy and
           satisfaction. Fast forward to today and I've had the privilege of
-          building software for a start-up and many side projects of my own.
+          building web and mobile applications in addition to many side projects of my own.
         </p>
         <p>
           The majority of my days are spent building and perfecting products,
@@ -62,23 +62,77 @@ const Home = () => {
 
         <h1 id="experience">Experience</h1>
         <table className="experience-container">
+        <tr>
+            <td id="experience-years">
+              <p>Jul 2024 - Present</p>
+            </td>
+            <td id="experience-description">
+              <p id="experience-role">Software Engineer</p>
+              <p id="experience-company-name">Upgrade Consulting · Remote</p>
+              <p>
+              </p>
+              <p>Projects:</p>
+              <p>
+                - GameOn: A sports betting app streaming real time game results using
+                webhooks and third party APIs (Odds and Stats.inc). Integrated web3 wallet
+                allowing users to deposite/withdraw Ethereum tokens.
+              </p>
+              <div id="experience-skills">
+                <span>Flutter</span>
+                <span>Dart</span>
+                <span>Node.js</span>
+                <span>Express.js</span>
+                <span>AWS</span>
+              </div>
+            </td>
+          </tr>
           <tr>
             <td id="experience-years">
               <p>Dec 2023 - Present</p>
             </td>
             <td id="experience-description">
               <p id="experience-role">Software Engineer</p>
-              <p id="experience-company-name">KLOTOFY · Remote</p>
-              <p>KLOTOFY is a fashion discovery app dedicated to help you find
+              <p id="experience-company-name">Klotofy · Remote</p>
+              <p>
+                Klotofy is a fashion discovery app dedicated to help you find
                 clothes that fit on your body, not the model on the screen.
               </p>
-              {/* <div id="experience-skills">
-                <span>React</span>
-                <span>Typescript</span>
-                <span>Spring Boot</span>
-                <span>PostgreSQL</span>
-                <span>Jooq</span>
-              </div> */}
+              <p>Notable projects:</p>
+              <p>
+                - Developed an algorithm to recommend users with the same body
+                type and measurements
+              </p>
+              <p>- Created a rewards system to assist marketing efforts of recruiting 
+                content creators by automating point distribution from in app activity
+              </p>
+              <div id="experience-skills">
+                <span>Flutter</span>
+                <span>FlutterFlow</span>
+                <span>Dart</span>
+                <span>Firebase</span>
+                <span>Google Cloud Platform</span>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td id="experience-years">
+              <p>Jun 2024 - Sep 2024</p>
+            </td>
+            <td id="experience-description">
+              <p id="experience-role">Software Engineer</p>
+              <p id="experience-company-name">Mentee · Remote</p>
+              <p>
+                Mentee is a service helping teenagers build habits and find
+                intrinsic motivation through psychology, technology, and
+                mentorship.
+              </p>
+              <div id="experience-skills">
+                <span>Flutter</span>
+                <span>FlutterFlow</span>
+                <span>Dart</span>
+                <span>Firebase</span>
+                <span>Google Cloud Platform</span>
+              </div>
             </td>
           </tr>
           <tr>
@@ -136,13 +190,12 @@ const Home = () => {
                 <Link
                   to="https://jaz-portfolio.vercel.app/"
                   target="_blank"
+                  id="project-name"
                 >
                   Jasmine Li &nbsp; <FiExternalLink />
                 </Link>
               </p>
-              <p>
-                Custom portfolio website for project management client.
-              </p>
+              <p>Custom portfolio website for project management client.</p>
               <div id="experience-skills">
                 <span>React.js</span>
               </div>
